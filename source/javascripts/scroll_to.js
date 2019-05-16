1010af5a068b98089f041db4a85919acef0a5ea6
Lars Bohm <larsbohm@gmail.com>
2019-05-16 12:59:50 +0200

    account for extra section

--- source/javascripts/scroll_to.js
+++ source/javascripts/scroll_to.js
@@ -5,7 +5,7 @@ function scrollToSection() {
   allButtons.forEach(function(item, index) {
     item.addEventListener('click', function(event) {
       event.preventDefault();
-      var nextSection = allSections[index];
+      var nextSection = allSections[index + 1];
       nextSection.scrollIntoView({behavior: "smooth"});
     })
   })

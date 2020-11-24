773da8b2902afd226492f6b8ccb76ef7e423a93d
Lars Bohm <larsbohm@gmail.com>
2020-11-24 18:59:36 +0100

    Automated commit at 2020-11-24 17:59:36 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/accordion-9e750300.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".accordion").forEach(function(e){e.addEventListener("click",function(e){e.currentTarget.classList.toggle("activeAccordion")})})});
